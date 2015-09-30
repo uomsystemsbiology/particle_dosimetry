30b3941ea399da6f0215b8e14d910d5b40e5e69d
matt-faria <mwffwm@gmail.com>
2015-09-30 17:42:00 +1000

    modifications to calculate upright cellular dose

--- matlab-src/calculate_upright_cellular_dose.m
+++ matlab-src/calculate_upright_cellular_dose.m
@@ -2,7 +2,7 @@ function calculate_upright_cellular_doses(size_range, density_range, workfile)
     particle_number = 4.5e8;
     fake_z_pot = -35.2e-3;
     
-    upright_24well_cond = ExperimentalCondition.StandardCondition(25*60*60, .0053, .022, .002, c.upright_cells);
+    upright_24well_cond = ExperimentalCondition.StandardCondition(24*60*60, .0053, .022, .002, c.upright_cells);
     boundary_condition = cell_boundary_conditions.constant_concentration_zero;
     
     len_densities = length(density_range);
@@ -12,17 +12,17 @@ function calculate_upright_cellular_doses(size_range, density_range, workfile)
     fprintf('Progress:\n');
     fprintf(['\n' repmat('.',1,len_sizes) '\n\n']);
     
-    for i=1:len_sizes
+    parfor i=1:len_sizes
         size = size_range(i);
         fprintf('\b|\n');
         for j=1:len_densities
             density = density_range(j);
-            disp([size, density]);
-            disp([i,j]);            
+            %disp([size, density]);
+            %disp([i,j]);
             particle = Particle('Test', size, density, fake_z_pot, 1);
             ec1 = ExperimentAndParticle(upright_24well_cond, particle);
-            %upright_r = diffusion_sedimentation_pde(ec1);
-            upright_r = diffusion_sedimentation_2d_pde(ec1, boundary_condition);
+            upright_r = diffusion_sedimentation_pde(ec1);
+            %upright_r = diffusion_sedimentation_2d_pde(ec1, boundary_condition);            
             upright_results(i,j) = upright_r.amount_removed;
         end
     end
--- matlab-src/driver_calculate_upright_cellular_dose.m
+++ matlab-src/driver_calculate_upright_cellular_dose.m
@@ -4,6 +4,10 @@ function [ ] = driver_calculate_upright_cellular_dose()
     
     size_range = cat(2, linspace(1e-9, 300e-9, 3), linspace(500e-9, 5000e-9, 3));
     density_range = cat(2, linspace(1001, 2000, 3), linspace(2000, 20000, 3));
+    size_range = [1e-9, 5e-9, 10e-9, 50e-9, 100e-9, 250e-9, 500e-9, 1000e-9, 2500e-9, 5000e-9]; 
+    density_range = [1001, 1250, 1500, 2000, 2500, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000];
+    
+    
     calculate_upright_cellular_dose(size_range, density_range, 'upright_cell_dose.mat')
 
 end
